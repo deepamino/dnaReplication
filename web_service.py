77778d3813ecc4ec66d66e80c9373f26d21cde15
Ricardo Cárdenes <78762064+ricardocardn@users.noreply.github.com>
2024-10-06 13:49:19 +0100

    API improvements

--- web_service.py
+++ web_service.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
 from data_loader import LoaderFactory
 from replication import Replication
+from dna import DNA
 
 
 app = Flask(__name__)
 
 
-@app.route('/seq', methods=['GET'])
+@app.route('/replica', methods=['GET'])
 def get_dna_seq():
     loader = LoaderFactory.initialize_loader('Files')
     id = request.args.get('id')
@@ -27,11 +28,9 @@ def replicate():
     rate = request.json.get('rate', 0.005)
     scale = request.json.get('scale', 20)
 
-    loader = LoaderFactory.initialize_loader('Files')
-    id = request.args.get('id')
-    dna_seq = loader.collect(f'{id}.dna')
-    replication = Replication().execute(dna_seq, rate=rate, scale=scale)
-    return jsonify({'text': replication, 'son1': replication[0], 'son2': replication[1]})
+    dna = DNA(dna_seq)
+    replication = Replication().execute(dna, rate=rate, scale=scale)
+    return jsonify({'son1': replication[0], 'son2': replication[1]})
 
 
 if __name__ == '__main__':
